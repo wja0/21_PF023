69b97bbd29599c32943a4f1f2d4b9d288607e6dd
wja0 <dnwkdud99@gmail.com>
2021-09-19 02:04:17 +0900

    파메 alarm 데이터 read & 리사이클러뷰 출력

---
+++ android app/app/src/main/java/com/example/probono/NoticeData.java
@@ -0,0 +1,35 @@
+package com.example.probono;
+
+public class NoticeData {
+
+    // 리사이클러뷰
+    private String title;
+    private String comment;
+    private String time;
+
+    public NoticeData(){}
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+}
