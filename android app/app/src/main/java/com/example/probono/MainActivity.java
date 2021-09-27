aeff3a196874f8ad10d1adc7d7bbfe730ad67f43
wja0 <dnwkdud99@gmail.com>
2021-09-27 16:15:16 +0900

    푸시알람

--- android app/app/src/main/java/com/example/probono/MainActivity.java
+++ android app/app/src/main/java/com/example/probono/MainActivity.java
@@ -1,14 +1,21 @@
 package com.example.probono;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.messaging.FirebaseMessaging;
+
 
 public class MainActivity extends AppCompatActivity {
+    private  String TAG = "FirebaseFCM";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,5 +54,21 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        FirebaseMessaging.getInstance().getToken()
+                .addOnCompleteListener(new OnCompleteListener<String>() {
+                    @Override
+                    public void onComplete(@NonNull Task<String> task) {
+                        if (!task.isSuccessful()) {
+                            Log.w("FCM Firebase Main", "Fetching FCM registration token failed", task.getException());
+                            return;
+                        }
+                        // Get new FCM registration token
+                        String token = task.getResult();
+                        // Log and toast
+                        String msg = getString(R.string.msg_token_fmt, token);
+                        Log.d("FCM Firebase Main", msg);
+                    }
+                });
     }
 }
